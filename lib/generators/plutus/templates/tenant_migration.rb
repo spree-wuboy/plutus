39c8cb9800bc1ecb612d56b548f6565353adedea
Frédéric Scheubel <fredsifu@hotmail.com>
2016-09-21 20:51:32 -0400

    Update tenancy migration with Entry modification

--- lib/generators/plutus/templates/tenant_migration.rb
+++ lib/generators/plutus/templates/tenant_migration.rb
@@ -2,5 +2,6 @@ class TenantPlutusTables < ActiveRecord::Migration
   def change
     # add a tenant column to plutus accounts table.
     add_column :plutus_accounts, :tenant_id, :integer, index: true
+    add_column :plutus_entries, :tenant_id, :integer, index: true
   end
 end
